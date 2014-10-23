63f8dac1c04fe45a5cea544bef9e0a82e5263843
jwncoexists <jenniferwnelson@sbcglobal.net>
2014-10-22 18:02:19 -0700

    Converted album view to Angular with zebra-striped song table.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -40,11 +40,17 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
      templateUrl: '/templates/collection.html'
    });
 
+   $stateProvider.state('album', {
+     url: '/album',
+     controller: 'Album.controller',
+     templateUrl: '/templates/album.html'
+   });
+
    $stateProvider.state('song', {
      url: '/song',
      controller: 'Song.controller',
      templateUrl: '/templates/song.html'
-   });   
+   });      
  }]);
  
  // This is a cleaner way to call the controller than crowding it on the module definition.
@@ -90,7 +96,38 @@ blocJams.controller('Collection.controller', ['$scope', function($scope) {
    }
  }]);
 
+blocJams.controller('Album.controller', ['$scope', function($scope) {
+   $scope.album = angular.copy(albumPicasso);
+   var hoveredSong = null;
+   var playingSong = null;
+ 
+   $scope.onHoverSong = function(song) {
+     hoveredSong = song;
+   };
+ 
+   $scope.offHoverSong = function(song) {
+     hoveredSong = null;
+   };
 
- blocJams.controller('Song.controller', ['$scope', function($scope) {
+   $scope.getSongState = function(song) {
+     if (song === playingSong) {
+       return 'playing';
+     }
+     else if (song === hoveredSong) {
+       return 'hovered';
+     }
+     return 'default';
+   };
+ 
+   $scope.playSong = function(song) {
+      playingSong = song;
+   };
+ 
+   $scope.pauseSong = function(song) {
+      playingSong = null;
+   };
+ }]);
+
+blocJams.controller('Song.controller', ['$scope', function($scope) {
 
  }]);
